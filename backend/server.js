2742fa09960ee88ae3ec483a5f263b8fc4ff69e0
manzoor wayar <softwareengineerwayar@gmail.com>
2022-03-02 11:15:37 +0430

    add some packages and middlewares.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import colors from "colors";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+// Route files
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
-// Config env vars
-dotenv.config();
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Mount routers
+app.use("/api/v1/auth", authRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// Error Handling Middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT | 5000;
 
